b02919beb32cc8d6d1299509eb8bc5202ab253e7
Akhil-star <akhilakki1305@gmail.com>
2020-10-16 15:23:45 +0530

    merge with Find max of three float

--- src/test/java/com/cg/generics/FindMaximumTest.java
+++ src/test/java/com/cg/generics/FindMaximumTest.java
@@ -6,6 +6,26 @@ import org.junit.Test;
 public class FindMaximumTest {
 
     FindMaximum findMaximum = new FindMaximum();
+
+    @Test
+    public void given3Integers_whenGiven_ShouldReturnFirstIntegerMax(){
+        Integer findMax = findMaximum.testMax(10,7,5);
+        Assert.assertSame( 10,findMax );
+    }
+
+    @Test
+    public void given3Integers_whenGiven_ShouldReturnSecondIntegerMax(){
+        Integer findMax = findMaximum.testMax(5,10,7);
+        Assert.assertSame( 10,findMax );
+
+    }
+
+    @Test
+    public void given3Integers_whenGiven_ShouldReturnThirdIntegerMax(){
+        Integer findMax = findMaximum.testMax(5,7,10);
+        Assert.assertSame( 10,findMax );
+    }
+    
     @Test
     public void given3Float_whenGiven_ShouldReturnFirstFloatMax(){
         Float findMax = findMaximum.testMax(10.4f,7.2f,5.5f);
