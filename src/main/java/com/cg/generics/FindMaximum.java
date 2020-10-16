0274fae44fa886f15127ecd1ae91aefe48620e55
Akhil-star <akhilakki1305@gmail.com>
2020-10-16 17:41:31 +0530

    Print max of elements

--- src/main/java/com/cg/generics/FindMaximum.java
+++ src/main/java/com/cg/generics/FindMaximum.java
@@ -3,30 +3,35 @@ import java.util.Arrays;
 
 public class FindMaximum <T extends Comparable<T>>{
 
-    T[] element;
+    T[] elements;
     public FindMaximum(T... elements){
-        this.element=elements;
+        this.elements=elements;
     }
 
     public T testMax(){
-        return FindMaximum.testMax( element );
+        return FindMaximum.testMax( elements );
     }
 
     public static <T extends Comparable<T>> T testMax(T... elements) {
-        T max = elements[0];
+        T max;
         if (elements == null) {
             System.out.println( "Could not find maximum" );
             return null;
         }else {
             Arrays.sort( elements );
-            return elements[elements.length-1];
+            max= elements[elements.length-1];
         }
+        printMax( max,elements );
+        return max;
     }
-    
-    public static void main (String args[]){
-        System.out.println(new FindMaximum( 10,12,7,2,15 ).testMax());
-        System.out.println(new FindMaximum( 10.2f,12.4f,7.8f ).testMax());
-        System.out.println(new FindMaximum( "Apple","Peach","Banana" ).testMax());
 
+    public static <T> void printMax(T max, T... elements){
+        System.out.println( "Maximum element is : "+max );
+    }
+
+    public static void main (String args[]){
+        new FindMaximum( 10,12,7,2,15 ).testMax();
+        new FindMaximum( 10.2f,12.4f,7.8f ).testMax();
+        new FindMaximum( "Apple","Peach","Banana" ).testMax();
     }
 }
