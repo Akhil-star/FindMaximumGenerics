1e4b2468696fa2d58430b4e0fe51e48308aa0f7b
Akhil-star <akhilakki1305@gmail.com>
2020-10-16 15:20:46 +0530

    Find max of three float

--- src/main/java/com/cg/generics/FindMaximum.java
+++ src/main/java/com/cg/generics/FindMaximum.java
@@ -3,6 +3,7 @@ package com.cg.generics;
 import java.util.Scanner;
 
 public class FindMaximum {
+
     public static Integer testMax(Integer intX,Integer intY,Integer intZ){
         Integer max = intX;
         if(intY.compareTo( max )>0){
@@ -14,8 +15,20 @@ public class FindMaximum {
         return max;
     }
 
+    public static Float testMax(Float floatX,Float floatY,Float floatZ){
+        Float max = floatX;
+        if(floatY.compareTo( max )>0){
+            max = floatY;
+        }
+        if(floatZ.compareTo( max )>0){
+            max = floatZ;
+        }
+        return max;
+    }
+
     public static void main (String args[]){
-        Scanner sc = new Scanner( System.in );
         System.out.println(testMax( 10,12,7 ));
+        System.out.println(testMax( 10.2f,12.4f,7.8f ));
+
     }
 }
--- src/test/java/com/cg/generics/FindMaximumTest.java
+++ src/test/java/com/cg/generics/FindMaximumTest.java
@@ -7,21 +7,22 @@ public class FindMaximumTest {
 
     FindMaximum findMaximum = new FindMaximum();
     @Test
-    public void given3Integers_whenGiven_ShouldReturnFirstIntegerMax(){
-        Integer findMax = findMaximum.testMax(10,7,5);
-        Assert.assertSame( 10,findMax );
+    public void given3Float_whenGiven_ShouldReturnFirstFloatMax(){
+        Float findMax = findMaximum.testMax(10.4f,7.2f,5.5f);
+        Assert.assertEquals( 10.4,findMax ,0.001);
     }
 
     @Test
-    public void given3Integers_whenGiven_ShouldReturnSecondIntegerMax(){
-        Integer findMax = findMaximum.testMax(5,10,7);
-        Assert.assertSame( 10,findMax );
+    public void given3Float_whenGiven_ShouldReturnSecondFloatMax(){
+        Float findMax = findMaximum.testMax(5.5f,10.4f,7.2f);
+        Assert.assertEquals( 10.4,findMax ,0.001);
 
     }
 
     @Test
-    public void given3Integers_whenGiven_ShouldReturnThirdIntegerMax(){
-        Integer findMax = findMaximum.testMax(5,7,10);
-        Assert.assertSame( 10,findMax );
+    public void given3Float_whenGiven_ShouldReturnThirdFloatMax(){
+        Float findMax = findMaximum.testMax(5.5f,7.2f,10.4f);
+        Float f=new Float( 10.4f );
+        Assert.assertEquals( f,findMax,0.001);
     }
 }
